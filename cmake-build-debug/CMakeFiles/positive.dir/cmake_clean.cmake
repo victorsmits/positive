cd54187b4c15159de1b493233282e2a03f81b2fa
victorsmits <victor.smits@student.vinci.be>
2018-11-21 11:59:20 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/positive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/positive.dir/main.c.o"
+  "positive.pdb"
+  "positive"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/positive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
